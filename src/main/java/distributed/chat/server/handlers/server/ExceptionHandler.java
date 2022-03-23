69f9cdbe421d40313ef151d6ae25e4b371164f70
mpmadhavig <madhavi.17@cse.mrt.ac.lk>
2022-03-23 17:27:15 +0530

    debug

--- src/main/java/distributed/chat/server/handlers/server/ExceptionHandler.java
+++ src/main/java/distributed/chat/server/handlers/server/ExceptionHandler.java
@@ -1,6 +1,5 @@
 package distributed.chat.server.handlers.server;
 
-import distributed.chat.server.service.election.ElectionService;
 import distributed.chat.server.states.ServerState;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
@@ -12,20 +11,19 @@ public class ExceptionHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) {
+        System.out.println("Channel Inactive detected");
         for (Map.Entry<String, Channel> server : ServerState.serverChannels.entrySet()) {
             Channel c = ServerState.serverChannels.get(server.getKey());
             if (c.equals(ctx.channel())) {
-                c.close();
+                ctx.close();
                 ServerState.serverChannels.remove(server.getKey());
                 System.out.println(server.getKey() + " channel closed");
-                // TODO : stop write requests
             }
-            if (server.getKey().equals(ServerState.leaderId)) {
-                System.out.println("Channel Inactive detected");
-                System.out.println(server.getKey() + " was the coordinator.");
-                ElectionService electionService = ElectionService.getInstance();
-                electionService.startElection();
-            }
+//            if (server.getKey().equals(ServerState.leaderId)) {
+//                System.out.println(server.getKey() + " was the coordinator.");
+//                ElectionService electionService = ElectionService.getInstance();
+//                electionService.startElection();
+//            }
         }
     }
 
