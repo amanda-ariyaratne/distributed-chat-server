b2ff954adb403dfbf7628e2b415f3af4ac5bb6b0
Amanda Ariyaratne <amanda.ariyaratne@protonmail.com>
2022-03-23 08:06:16 +0530

    update heartbeat timeout

--- src/main/java/distributed/chat/server/states/ServerState.java
+++ src/main/java/distributed/chat/server/states/ServerState.java
@@ -58,7 +58,7 @@ public class ServerState {
     public static int coordinatorTimeout = 5000;
     public static int nominatorOrCoordinatorTimeout = 5000;
 
-    public static int heartBeatReadTimeout = 5000;
-    public static int heartBeatWriteTimeout = 4000;
+    public static int heartBeatReadTimeout = 5;
+    public static int heartBeatWriteTimeout = 4;
 
 }
